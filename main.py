a58ec78a583b7b20ad197793b9cd7e4789f34360
Quintin Bland <quintinbland2@gmail.com>
2022-10-25 11:16:52 -0700

    adding checks for winnings and total balance

--- main.py
+++ main.py
@@ -22,7 +22,20 @@ symbol_value = {
 }
 
 def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
     for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+
+    return winnings, winning_lines
+
         
 
 def get_slot_machine_spin(rows, cols, symbols):
@@ -50,8 +63,7 @@ def print_slot_machine(columns):
             if i != len(columns) - 1:
                 print(column[row], end=" | ")
             else:
-                print(column[row], end="")
-
+                print(column[row], end="")        
         print()
 
 def deposit():
@@ -94,8 +106,7 @@ def get_bet():
     return amount
 
 
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -111,5 +122,20 @@ def main():
 
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won ${winnings}.")
+    print(f"You won on lines:", *winning_lines)
+    return winnings - total_bet
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is ${balance}")
+        answer = input("Press enter to spin (q to quit).")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You left with ${balance}")
 
 main()
